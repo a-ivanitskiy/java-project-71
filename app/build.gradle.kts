26db2b7b8c20778e9f3cf3f8cbbb242517f76f78
Anton Ivanitskiy <anton@ivanitskiy.com>
2024-06-02 12:14:10 +0300

    init project

---
+++ app/build.gradle.kts
@@ -0,0 +1,32 @@
+var lombokVersion = "1.18.32"
+var checkstyleVersion = "10.12.4"
+
+plugins {
+    id("java")
+    application
+    checkstyle
+}
+
+group = "hexlet.code"
+version = "0.1-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    compileOnly("org.projectlombok:lombok:$lombokVersion")
+    annotationProcessor("org.projectlombok:lombok:$lombokVersion")
+
+    testCompileOnly("org.projectlombok:lombok:$lombokVersion")
+    testAnnotationProcessor("org.projectlombok:lombok:$lombokVersion")
+}
+
+tasks.test { useJUnitPlatform() }
+
+application { mainClass = "hexlet.code.App" }
+
+checkstyle { toolVersion = checkstyleVersion }
